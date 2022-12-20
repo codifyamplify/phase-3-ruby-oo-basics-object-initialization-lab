c73faca14bf0c5c0569f2fcc4d7414bc6c8193c7
David Whitney <codifyamplify@gmail.com>
2022-12-19 19:55:22 -0500

    commit tree d7c8cfe377b596fca56a7714db997b9c7f5b27c3

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+    # write an initialization method for the dogs name that accepts an argument for the name
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+    # write an initialize method that accepts an argumen for the person's name
+    def initialize(name)
+        @name = name
+    end
+end
